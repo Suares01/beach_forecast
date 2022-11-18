7b1a056406626149b63d4a353543c2726bbc2ad8
Lucas Suares <hub_suares.01@hotmail.com>
2022-11-18 00:16:53 -0300

    feat(api): setup vite multi-page app

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
@@ -5,4 +6,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
